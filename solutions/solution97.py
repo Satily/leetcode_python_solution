4aba1c66fbdaa40f5f52574f24de01d2df5dbe20
satily <houjiaxu@xiaomi.com>
2018-11-01 10:56:58 +0800

    Solution 97

--- solutions/solution97.py
+++ solutions/solution97.py
@@ -7,11 +7,18 @@ class Solution:
         :rtype: bool
         """
         ls1, ls2, ls3 = len(s1), len(s2), len(s3)
+        if ls1 + ls2 != ls3:
+            return False
         dp = [[False] * (ls2 + 1) for _ in range(0, ls1 + 1)]
         dp[0][0] = True
-        for is1 in range(0, ls1):
-            for is2 in range(0, ls2):
-                pass
+        for is1 in range(0, ls1 + 1):
+            for is2 in range(0, ls2 + 1):
+                if dp[is1][is2]:
+                    if is1 < ls1 and s1[is1] == s3[is1 + is2]:
+                        dp[is1 + 1][is2] = True
+                    if is2 < ls2 and s2[is2] == s3[is1 + is2]:
+                        dp[is1][is2 + 1] = True
+        return dp[ls1][ls2]
 
 
 if __name__ == "__main__":
