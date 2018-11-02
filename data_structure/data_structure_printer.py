add82fd0cfb3091bcbb3f0bfe2b6e3cd20ea016b
satily <houjiaxu@xiaomi.com>
2018-11-03 01:37:19 +0800

    Update ds_print

--- data_structure/data_structure_printer.py
+++ data_structure/data_structure_printer.py
@@ -1,13 +1,14 @@
-from data_structure.link_list import ListNode
-from data_structure.link_list import flatten_link_list
-from data_structure.binary_tree import TreeNode
-from data_structure.binary_tree import flatten_binary_tree
+from data_structure.link_list import ListNode, flatten_link_list
+from data_structure.binary_tree import TreeNode, flatten_binary_tree
 
 
-def ds_print(structure):
-    transformed_structure = None
-    if isinstance(structure, ListNode):
-        transformed_structure = flatten_link_list(structure)
-    elif isinstance(structure, TreeNode):
-        transformed_structure = flatten_binary_tree(structure)
-    print(transformed_structure)
+def ds_print(*args):
+    def transform(structure):
+        transformed_structure = structure
+        if isinstance(structure, ListNode):
+            transformed_structure = flatten_link_list(structure)
+        elif isinstance(structure, TreeNode):
+            transformed_structure = flatten_binary_tree(structure)
+        return transformed_structure
+    print(*tuple(map(transform, args)))
+
