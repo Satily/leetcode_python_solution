9d9a1353cc2b3f3572338fc6c028f29a5f4f73c5
satily <houjiaxu@xiaomi.com>
2018-11-21 18:00:02 +0800

    Solution 10

--- solutions/solution10.py
+++ solutions/solution10.py
@@ -1,3 +1,9 @@
+class Character(object):
+    def __init__(self, character, number):
+        self.character = character
+        self.number = number
+
+
 class Solution:
     def isMatch(self, s, p):
         """
@@ -5,27 +11,35 @@ class Solution:
         :type p: str
         :rtype: bool
         """
-        ls, lp = len(s), len(p)
-        # init
-        dp = [[False] * (lp + 1) for _ in range(0, ls + 1)]
-        for i in range(0, ls + 1):
-            pass
-        for i in range(0, lp + 1):
-            pass
+        lp, ls = len(p), len(s)
+        index = 0
+        patten = []
+        while index < lp:
+            character = p[index]
+            if index + 1 < lp and p[index + 1] == "*":
+                number = 0
+                index += 1
+            else:
+                number = 1
+            patten.append(Character(character, number))
+            index += 1
+        lp = len(patten)
+        dp = [[False] * (lp + 1) for _ in range(ls + 1)]
         dp[0][0] = True
-        for pi in range(1, lp + 1):
-            for si in range(1, ls + 1):
-                if pi != 0 or si != 0:
-                    if p[pi - 1] == '.' and dp[si - 1][pi - 1]:
-                        dp[si][pi] = True
-                    elif p[pi - 1] == '*' and (dp[si - 1][pi - 1] or dp[si - 1][pi]):
-                        dp[si][pi] = True
-                    elif p[pi - 1] == s[si - 1] and dp[si - 1][pi - 1]:
-                        dp[si][pi] = True
+        for index_s in range(ls + 1):
+            for index_p in range(lp):
+                if dp[index_s][index_p]:
+                    if index_s < ls and (patten[index_p].character == '.' or patten[index_p].character == s[index_s]):
+                        dp[index_s + 1][index_p + 1] = True
+                        if patten[index_p].number == 0:
+                            dp[index_s + 1][index_p] = True
+                    if patten[index_p].number == 0:
+                        dp[index_s][index_p + 1] = True
         return dp[ls][lp]
 
 
 if __name__ == "__main__":
+    print(Solution().isMatch('a', 'ab*'))
     print(Solution().isMatch('aa', 'a'))
     print(Solution().isMatch('aa', 'a*'))
     print(Solution().isMatch('ab', '.*'))
