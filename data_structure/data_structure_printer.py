8192975542cc661493fbdd7d09a5516b47224930
satily <ladfmcl@126.com>
2019-07-29 19:35:30 +0800

    Solution951

--- data_structure/data_structure_printer.py
+++ data_structure/data_structure_printer.py
@@ -2,13 +2,13 @@ from data_structure.link_list import ListNode, flatten_link_list
 from data_structure.binary_tree import TreeNode, flatten_binary_tree
 
 
-def ds_print(*args):
+def ds_print(*args, mode=None):
     def transform(structure):
         transformed_structure = structure
         if isinstance(structure, ListNode):
             transformed_structure = flatten_link_list(structure)
         elif isinstance(structure, TreeNode):
-            transformed_structure = flatten_binary_tree(structure)
+            transformed_structure = flatten_binary_tree(structure, mode=mode)
         return transformed_structure
     print(*tuple(map(transform, args)))
 
