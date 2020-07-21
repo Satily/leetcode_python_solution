8f668534d94207bddc6629cd8f43c4d5e0032d3c
satily <ladfmcl@126.com>
2020-07-22 00:17:41 +0800

    Solution95

---
+++ solutions/solution95.py
@@ -0,0 +1,31 @@
+from data_structure import TreeNode, ds_print
+
+
+class Solution:
+    def generateTrees(self, n: int) -> 'List[TreeNode]':
+
+        def generate_tree_with_range(start, end):
+            if start > end:
+                return [None]
+            result = []
+            for index in range(start, end + 1):
+                left_trees = generate_tree_with_range(start, index - 1)
+                right_trees = generate_tree_with_range(index + 1, end)
+                for left_tree in left_trees:
+                    for right_tree in right_trees:
+                        this_node = TreeNode(index)
+                        this_node.left, this_node.right = left_tree, right_tree
+                        result.append(this_node)
+            return result
+
+        if n == 0:
+            return []
+        else:
+            return generate_tree_with_range(1, n)
+
+
+if __name__ == '__main__':
+    for tree in Solution().generateTrees(3):
+        ds_print(tree)
+    # for tree in Solution().generateTrees(0):
+    #     ds_print(tree)
